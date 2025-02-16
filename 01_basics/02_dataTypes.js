bce834855c1d4207c77694063c840a89dd9154d1
uditiraina <uditiraina10@gmail.com>
2025-02-16 09:29:58 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+ "use strict"; //treat all JS code as newer version
+
+ // alert(3+3)// we are using node js and not browser
+
+ console.log(3
+    +3) // code readability should be high
+
+ console.log("Uditi")
+
+ let name = "uditi"
+ let age = 21
+ let isLoggedIn = false
+ let state;
+
+ // DATATYPES(premetive datatypes)
+ // number => 2 to power 53
+ // bigint
+ // string => " "
+ // boolean=> true /false
+ // null => standalone value(representation of empty value)
+ /* undefined => jo assign toh ho jaate hai magar unme koi
+ value nahi store kri hoti hai humne */
+ // symbol => unique
+
+ // object
+
+ console.log(typeof "uditi")
+ console.log(typeof undefined)
