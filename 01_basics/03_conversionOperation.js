fe18117986490d074c0d6ceaac6ef8bbcb456b11
uditiraina <uditiraina10@gmail.com>
2025-02-17 18:35:13 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+
+// let score = uditi
+
+// console.log(typeof score);
+//console.log(typeof(score));
+
+// if score = "33" likha hota toh humara output me aata string 
+
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber)
+// value in number me output aa rha hai NaN what is that? 
+
+//"33" => 33
+//"33abc" => NaN
+// true=> 1; false=> 0
+
+/* let isLoggedIn = true
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn)
+*/
+// 1 => true; 0 => false
+// " " => false
+// "uditi" => true
+
+let someNumber = 33
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
