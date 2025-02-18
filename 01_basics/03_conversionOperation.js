c908078e0cf6da1fca02f7b1acc33f63888154ef
uditiraina <uditiraina10@gmail.com>
2025-02-18 04:53:52 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,47 @@
 
 let someNumber = 33
 let stringNumber= String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**************** operations**********************//
+
+let value =3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = "uditi"
+let str3 = str1 + str2
+/*console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");*/
+
+//console.log((3+4)*5%3);  // use brackets for your as 
+//.                       well as systems understanding
+
+/*console.log(+true)
+// output = 1
+console.log(true+)
+// gives error*/
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// prefix and postfix js mdn search kro 
