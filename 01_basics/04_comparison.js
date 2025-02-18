94fb3c17a03106ccda7f909d96ed597a38043c2e
uditiraina <uditiraina10@gmail.com>
2025-02-18 05:04:36 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+/*here we are comparing same data types so there is no issue we get our answers in boolean were it tells ki answer true hai ya false
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+*/
+
+// console.log("2">1); // predictible result nahi aata hai jab bhi hum do alag data types ko compare krte hai 
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null ==0);
+console.log(null>=0);
